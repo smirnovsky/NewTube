2433c2fe457e9dbbc51d65875ffcc955e0a0e4a5
smirnovsky <happy280190@gmail.com>
2023-01-09 12:41:32 +0300

    feat: add basic project structure and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components'
+
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetails />} />
+                <Route path="/video/:id" element={<ChannelDetails />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
   );
-}
 
-export default App;
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as SearchFeed } from './SearchFeed';
+export { default as VideoDetails } from './VideoDetails';
+export { default as ChannelDetails } from './ChannelDetails';
