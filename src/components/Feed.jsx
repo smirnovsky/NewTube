bde97ae2134729842e87eda1729078edb26fb983
smirnovsky <happy280190@gmail.com>
2023-01-09 14:35:01 +0300

    feat: add styles in feed markup

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -14,6 +14,11 @@ const Feed = () => {
           Alex Smirnov 2023
         </Typography>
       </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }} >
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+         New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
