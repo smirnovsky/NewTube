9b490c92e6f9fce7b3d4f84572b99b99709c0828
smirnovsky <happy280190@gmail.com>
2023-01-11 16:13:12 +0300

    feat: add channelcard component

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,32 @@
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import { demoProfilePicture } from '../utils/costants';
+
+const ChannelCard = ({ channelDetail }) => {
+    return (
+        <Box
+        sx={{
+            boxShadow: 'none',
+            borderRadius: '20px'
+        }}
+    >
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
+                <CardMedia 
+                    image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                    alt={channelDetail?.snippet?.title}
+                    sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+                />
+                <Typography variant='h6'>
+                    {channelDetail?.snippet?.title}
+                    <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
+                </Typography>
+            </CardContent>
+        </Link>
+    </Box>
+    )
+}
+
+export default ChannelCard
