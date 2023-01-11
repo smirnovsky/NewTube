34d8444d1e70cc35807f28c94263a7ca7a211959
smirnovsky <happy280190@gmail.com>
2023-01-11 16:29:42 +0300

    feat: add styles for ChannelCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,7 +9,13 @@ const ChannelCard = ({ channelDetail }) => {
         <Box
         sx={{
             boxShadow: 'none',
-            borderRadius: '20px'
+            borderRadius: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: { xs: '356px', md: '320px' },
+            height: '326px',
+            margin: 'auto'
         }}
     >
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -23,6 +29,11 @@ const ChannelCard = ({ channelDetail }) => {
                     {channelDetail?.snippet?.title}
                     <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
                 </Typography>
+                {channelDetail?.statistics?.subscriberCount && (
+                    <Typography>
+                        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+                    </Typography>
+                )}
             </CardContent>
         </Link>
     </Box>
