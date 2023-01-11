f57194a16ce6fb6bf8d081bcd70ba2a3f6ab7a3c
smirnovsky <happy280190@gmail.com>
2023-01-11 22:35:28 +0300

    fix: fixed styles

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,12 +7,14 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
 
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }} >
+    <Card sx={{ width: {  xs: '100%', sm: '358px', md: '270px' }, boxShadow: 'none', borderRadius: 0 }} >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url}
                 alt={snippet?.title}
-                sx={{ width: 320, height: 180 }}
+                sx={{ width: {
+                    xs: '100%', sm: '358px', md: '270px'
+                }, height: 180 }}
                 />
         </Link>
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} >
